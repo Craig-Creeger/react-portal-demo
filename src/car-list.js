9dea71a85c3aad4a798cbbecb80fb461b7b91b2b
Craig Creeger <Craig.Creeger@bestbuy.com>
2018-08-24 14:14:01 -0500

    Changes car list to an array

--- src/car-list.js
+++ src/car-list.js
@@ -4,24 +4,22 @@ import Swipeable from "./swipeable";
 
 import "./car-list.css";
 
+const cars = [
+  { name: "Mazda MX-5 RF", filename: "mazda_mx-5_rf.jpg" },
+  { name: "Austin-Healey 100 BN2", filename: "austin-healey_100_BN2.jpg" },
+  { name: "Aston Martin DB4 GT Zagato", filename: "Aston-Martin-DB4-GT-Zagato.jpg" }
+];
+
 function CarList() {
   return (
     <ul className="carList">
-      <li>
-        <Swipeable>
-          <Car carName="Mazda MX-5 RF" imgFilename="mazda_mx-5_rf.jpg" />
-        </Swipeable>
-      </li>
-      <li>
-        <Swipeable>
-          <Car carName="Austin-Healey 100 BN2" imgFilename="austin-healey_100_BN2.jpg" />
-        </Swipeable>
-      </li>
-      <li>
-        <Swipeable>
-          <Car carName="Aston Martin DB4 GT Zagato" imgFilename="Aston-Martin-DB4-GT-Zagato.jpg" />
-        </Swipeable>
-      </li>
+      {cars.map(car => (
+        <li key={car.name}>
+          <Swipeable>
+            <Car carName={car.name} imgFilename={car.filename} />
+          </Swipeable>
+        </li>
+      ))}
     </ul>
   );
 }
