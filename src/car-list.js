be4244490c9cd8d8c2d0720ebbb6deee0b50103b
Craig Creeger <Craig.Creeger@bestbuy.com>
2018-08-24 16:23:46 -0500

    Adds more cars to the list

--- src/car-list.js
+++ src/car-list.js
@@ -8,7 +8,15 @@ import "./car-list.css";
 const cars = [
   { name: "Mazda MX-5 RF", filename: "mazda_mx-5_rf.jpg" },
   { name: "Austin-Healey 100 BN2", filename: "austin-healey_100_BN2.jpg" },
-  { name: "Aston Martin DB4 GT Zagato", filename: "Aston-Martin-DB4-GT-Zagato.jpg" }
+  { name: "AMC Gremlin", filename: "amc_gremlin.jpg" },
+  { name: "Aston Martin DB4 GT Zagato", filename: "Aston-Martin-DB4-GT-Zagato.jpg" },
+  { name: "AMC Pacer", filename: "amc_pacer.jpg" },
+  { name: "Ford GT40", filename: "ford_gt40.jpg" },
+  { name: "Pontiac Aztek", filename: "pontiac_aztek.jpg" },
+  { name: "Jaguar E-type", filename: "jaguar_e-type.jpg" },
+  { name: "Nissan Leaf", filename: "nissan_leaf.jpg" },
+  { name: "Lotus Elise", filename: "lotus-elise.jpg" },
+  { name: "Seat Altea Freetrack", filename: "seat_altea_freetrack.jpg" }
 ];
 
 function CarList(props) {
